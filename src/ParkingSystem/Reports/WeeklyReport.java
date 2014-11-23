296b748b690546414ebefa81133fd97781f08981
BhavinP <bhaveen@gmail.com>
2014-11-22 18:30:47 -0700

    Added method to WeeklyReportData.

--- src/ParkingSystem/Reports/WeeklyReport.java
+++ src/ParkingSystem/Reports/WeeklyReport.java
@@ -1,5 +1,6 @@
 package ParkingSystem.Reports;
 
+import java.util.Date;
 import java.util.List;
 
 import ParkingSystem.Entities.Ticket;
@@ -7,9 +8,10 @@ import ParkingSystem.Entities.Ticket;
 public class WeeklyReport implements Report {
 
 	@Override
-	public List<Ticket> getReport(List<Ticket> allTickets) {
-
-		return allTickets;
+	public List<HourlyData> getReport(List<Ticket> allTickets,Date dt) {
+		 List<HourlyData>  weeklyData=null;
+		 
+		return weeklyData;
 	}
 
 }
