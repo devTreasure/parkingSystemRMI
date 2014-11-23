29727b954e6a6567bc7993ea9202e36466ed1dd0
BhavinP <bhaveen@gmail.com>
2014-11-22 18:50:03 -0700

    Added new entity  reportCollection

---
+++ src/ParkingSystem/Reports/ReportCollection.java
@@ -0,0 +1,72 @@
+package ParkingSystem.Reports;
+
+import java.util.Date;
+import java.util.UUID;
+
+public class ReportCollection {
+
+
+
+
+private static final long serialVersionUID = 1L;
+private String CCNumner;
+private String expiryDate;
+private int cvvNumber;
+private double amount;
+private Date creditcardpaymentTime;
+
+public double getAmount() {
+	return amount;
+}
+
+public void setAmount(double amount) {
+	this.amount = amount;
+}
+
+private UUID ticketID;
+
+public ReportCollection() {
+
+}
+
+public String getCCNumner() {
+	return CCNumner;
+}
+
+public void setCCNumner(String cCNumner) {
+	CCNumner = cCNumner;
+}
+
+public String getExpiryDate() {
+	return expiryDate;
+}
+
+public void setExpiryDate(String expiryDate) {
+	this.expiryDate = expiryDate;
+}
+
+public int getCvvNumber() {
+	return cvvNumber;
+}
+
+public void setCvvNumber(int cvvNumber) {
+	this.cvvNumber = cvvNumber;
+}
+
+public UUID getTicketID() {
+	return ticketID;
+}
+
+public void setTicketID(UUID ticketID) {
+	this.ticketID = ticketID;
+}
+
+public Date getCreditcardpaymentTime() {
+	return creditcardpaymentTime;
+}
+
+public void setCreditcardpaymentTime(Date creditcardpaymentTime) {
+	this.creditcardpaymentTime = creditcardpaymentTime;
+}
+
+}
