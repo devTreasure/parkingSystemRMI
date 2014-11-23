b0680f675ec8e12af3f6f49731103eb3f0706f83
BhavinP <bhaveen@gmail.com>
2014-11-22 20:37:56 -0700

    Refactored Entity - Cash.

--- src/ParkingSystem/Entities/Cash.java
+++ src/ParkingSystem/Entities/Cash.java
@@ -5,35 +5,36 @@ import java.util.UUID;
 
 public class Cash {
 
-	
 	private UUID TicketID;
 	private double Amount;
 	private Date paymentDate;
-	
-	public Cash()
-	{
-		
+
+	public Cash() {
+
 	}
-	
-		
+
 	public UUID getTicketID() {
 		return TicketID;
 	}
+
 	public void setTicketID(UUID ticketID) {
 		TicketID = ticketID;
 	}
+
 	public double getAmount() {
 		return Amount;
 	}
+
 	public void setAmount(double amount) {
 		Amount = amount;
 	}
+
 	public Date getPaymentDate() {
 		return paymentDate;
 	}
+
 	public void setPaymentDate(Date paymentDate) {
 		this.paymentDate = paymentDate;
 	}
-	
-	
+
 }
