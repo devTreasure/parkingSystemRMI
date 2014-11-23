17f6f3c9836c5819a8f7b648bc82136ec856a8cf
BhavinP <bhaveen@gmail.com>
2014-11-22 18:16:00 -0700

    Refactored Report interface for  to included start and end dates for reporting purpose.

--- src/ParkingSystem/Reports/Report.java
+++ src/ParkingSystem/Reports/Report.java
@@ -1,11 +1,12 @@
 package ParkingSystem.Reports;
 
+import java.util.Date;
 import java.util.List;
 
 import ParkingSystem.Entities.Ticket;
 
 public interface Report {
 	
-	public List<Ticket> getReport(List<Ticket> allTickets);
+	public List<HourlyData> getReport(List<Ticket> allTickets, Date dt);
 
 }
