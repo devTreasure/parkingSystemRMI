67b6e4beb7779ba39df851f1a0c07dd68e2628cc
BhavinP <bhaveen@gmail.com>
2014-11-22 19:09:16 -0700

    Added method processCashForParkingFeePayment  in paymentManagement

--- src/ParkingSystem/controller/PaymentManagement.java
+++ src/ParkingSystem/controller/PaymentManagement.java
@@ -2,10 +2,16 @@ package ParkingSystem.controller;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
+import ParkingSystem.Entities.Cash;
 import ParkingSystem.Entities.CreditCard;
+import ParkingSystem.Entities.Status;
 import ParkingSystem.Entities.Ticket;
+import ParkingSystem.Entities.paymentStratagy;
+import ParkingSystem.Reports.ReportCollection;
 
 public class PaymentManagement  implements Serializable {
 
@@ -15,9 +21,60 @@ public class PaymentManagement  implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	private CreditCard creditCard;
+	
+	private Cash  cashpayment;
 
 	private int hourlyRate;
 	private List<CreditCard> creditcollection = new ArrayList<CreditCard>();
+	
+	
+	private List<Cash> cashPaymentcollection = new ArrayList<Cash>();
+
+
+	public List<Cash> getCashPaymentcollection() {
+		return cashPaymentcollection;
+	}
+
+
+	public void setCashPaymentcollection(Cash cash) {
+		this.cashPaymentcollection.add(cash) ;
+	}
+
+	private List<ReportCollection> creditReportcollection = new ArrayList<ReportCollection>();
+	
+	//private paymentStratagy  strategy; 
+    
+	public void setCreditcollection(CreditCard creditcollection)
+	{
+		this.creditcollection.add(creditcollection);
+	}
+	
+
+	
+	public List<ReportCollection> getCreditReportcollection() {
+		return creditReportcollection;
+	}
+
+	public void setCreditReportcollection(ReportCollection  rpt) {
+		this.creditReportcollection.add(rpt);
+	}
+
+	public List<CreditCard> getCreditcollection() {
+		return creditcollection;
+	
+	}
+
+
+	
+	
+	public List<Cash> getPaymentcollection() {
+		return cashPaymentcollection;
+	}
+
+	public TransactionManagement getTransactionManager() {
+		return transactionManager;
+	}
+
 	public TransactionManagement transactionManager = new TransactionManagement();
 
 	public PaymentManagement() {
@@ -40,18 +97,71 @@ public class PaymentManagement  implements Serializable {
 		this.hourlyRate = hourlyRate;
 	}
 
-	public void processForParkingFeePayment(Ticket ticket, CreditCard card)
+	public Status processForParkingFeePayment(Ticket ticket, CreditCard card)
 
 	{
+		ReportCollection c=new ReportCollection();
+		
+		 c.setCCNumner(card.getCCNumner());
+		 c.setTicketID(card.getTicketID());
+		 c.setAmount(card.getAmount());
+		 c.setCreditcardpaymentTime(card.getCreditcardpaymentTime());
+		 
+		 
+		 
+		creditReportcollection.add(c);
+		
 		creditcollection.add(card);
-	
+		
+		Status isSuccessfull=null;
+		
 		if (ticket.getTicketAmount() > 0) {
 			card.setAmount(ticket.getTicketAmount());
-			Boolean isSuccessfull = transactionManager.ProcessTheTransaction(card);
+			isSuccessfull = transactionManager.ProcessTheTransaction(card);
 		}
 		ticket.setTicketAmount(0);// amount paid and due is set 0
 
 		ticket.setIsPaid(true);
+   
+		return isSuccessfull;
+	}
+	
+	
+	public Status processCashForParkingFeePayment(Ticket ticket)
+
+	{
+		//creditcollection.add(card);
+		Status isSuccessful=null;
+		
+		Calendar  cal= Calendar.getInstance();
+		
+		Date paymentdate=cal.getTime();
+		
+	    Cash objCash=new Cash();
+	    
+	    objCash.setPaymentDate(paymentdate);
+	    
+	   // if(ticket.amount>0)
+	   //   objCash.setAmount(amount);
+	    
+		if (ticket.getTicketAmount() > 0)
+		{
+			objCash.setTicketID(ticket.getTicektID());
+			objCash.setAmount(ticket.getTicketAmount());
+			
+			cashPaymentcollection.add(objCash);
+			
+			//Boolean isSuccessfull=true;
+			isSuccessful = new Status(true, "Payment has been successful");
+			
+			//Boolean isSuccessfull = transactionManager.ProcessTheTransaction(card);
+		}
+		
+		ticket.setTicketAmount(0);// amount paid and due is set 0
+
+		ticket.setIsPaid(true);
+		
+		return isSuccessful;
 
 	}
 
